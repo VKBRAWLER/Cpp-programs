837cbe71c1abbbb97c2a14de0b03312cf0179397
Varun Kharkwal <varunkh12345@gmail.com>
2023-12-05 22:08:19 +0530

    Add student class with two constructors and create objects

--- 03_NOVEMBER/ass04/14.cpp
+++ 03_NOVEMBER/ass04/14.cpp
@@ -1,1 +1,18 @@
 // Create the student class. Create two constructors for this class. One with parameters and other without parameters. Create two objects using different constructors
+#include<iostream>
+using namespace std;
+class student {
+  int rollno, marks;
+  public:
+  student () {
+    cout << "Obj created" << endl;
+  }
+  student (int a):
+  rollno(a)
+  {
+    cout << "Obj created with roll no.: " << rollno << endl;
+  }
+};
+int main() {
+  student obj1, obj2(1);
+}
