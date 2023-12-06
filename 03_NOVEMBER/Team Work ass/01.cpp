b0b5ffe546f595d8c75502af9841f09e1817f17d
Varun Kharkwal <varunkh12345@gmail.com>
2023-12-06 21:26:54 +0530

    Add Product class and implement basic functionality

---
+++ 03_NOVEMBER/Team Work ass/01.cpp
@@ -0,0 +1,79 @@
+#include<iostream>
+using namespace std;
+class Product {
+  char Product_Id;
+  string Product_Name;
+  int Total_sale;
+  char Product_Grade;
+  int position;
+  Product* next;
+  public:
+  static Product* front;
+  static Product* rear;
+  static int count;
+  Product () {
+    if (count >= 5) { cout << "Stack Overflow" << endl; return; }
+    if (front) { front->next = this; }
+    front = this;
+    if (!rear) { rear = this;}
+    count++;
+    this->position = count;
+    this->Insert();
+   }
+  void Insert () {
+    cout << "Enter Product ID ";
+    cin >> Product_Id;
+    // cout << "Enter Product Name ";
+    // cin >> Product_Name;
+    // cout << "Enter Total Sale ";
+    // cin >> Total_sale;
+    // cout << "Enter Product Grade ";
+    // cin >> Product_Grade;
+    next = NULL;
+    cout << "count: " << count << " front: " << front << " rear: " << rear << endl;
+  }
+  static void Delete () {
+    if (!realcount()) { cout << "Queue Underflow" << endl; return; }
+    cout << "Item " << rear->Product_Id << " poped out " << endl;
+    rear = rear->next;
+  }
+  static int realcount() {
+    int i = 0;
+    Product* p = rear;
+    while (p != NULL) {
+      i++;
+      p = p->next;
+    }
+    return i;
+  }
+  static void dis() {
+    Product* p = rear;
+    while (p != NULL) {
+      cout << " " << p->Product_Id << " ->";
+      p = p->next;
+    }
+    cout << " NULL" << endl;
+  }
+};
+Product* Product::front;
+Product* Product::rear;
+int Product::count = 0;
+int main() {
+  int choice;
+  Product* p;
+  while (1) {
+    cout << "Enter your choice: ";
+    cin >> choice;
+    switch (choice) {
+    case 1:
+      p = new Product;
+      break;
+    case 2:
+      Product::dis();
+      break;
+    default:
+      exit(1);
+      break;
+    }
+  }
+}
